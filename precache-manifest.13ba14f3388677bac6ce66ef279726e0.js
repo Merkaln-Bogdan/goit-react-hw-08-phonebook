d58153344ad6d612e0baa41ef724dcad0f007e89
IlkaiGundogun <piccasoul19@ukr.net>
2020-09-24 20:22:54 +0300

    Updates

---
+++ precache-manifest.13ba14f3388677bac6ce66ef279726e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7d5b9219521ffc0fe856c3b4a1f3ab",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "282478394a296cc02f6a",
+    "url": "/goit-react-hw-08-phonebook/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "46c6d5155672e146dacc",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.afb0e0a6.chunk.css"
+  },
+  {
+    "revision": "282478394a296cc02f6a",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.edee93b8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.edee93b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c6d5155672e146dacc",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.0d6dd405.chunk.js"
+  },
+  {
+    "revision": "2a13091ca041de263bee",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.650e6e2b.js"
+  },
+  {
+    "revision": "3b704aed559f5e98d2174d240cae343d",
+    "url": "/goit-react-hw-08-phonebook/static/media/logout.3b704aed.png"
+  },
+  {
+    "revision": "54e9ec5365eeb967838ffd2a35eda50b",
+    "url": "/goit-react-hw-08-phonebook/static/media/png-avatar.54e9ec53.png"
+  }
+]);
